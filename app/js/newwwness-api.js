a85ef2cf61cb430f4e3920fb3f56c8ac81fe0de4
David Blurton <david.blurton@gmail.com>
2015-02-19 10:00:46 +0000

    Add martini and cors

--- app/js/newwwness-api.js
+++ app/js/newwwness-api.js
@@ -2,7 +2,7 @@ import request from 'qwest'
 
 class NewwwnessApi {
   constructor() {
-    this.url = "https://gist.githubusercontent.com/davidblurton/f0628b01afc806a1c166/raw/9783ce2a9f5374e73e7b75ea7f6151b705658c86/gistfile1.json"
+    this.url = "http://newwwness-api.appspot.com/"
   }
   load() {
     return request.get(this.url)
