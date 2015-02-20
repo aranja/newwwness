c2d599f990454c5bdae29cc657e4199bb08e1598
David Blurton <david.blurton@gmail.com>
2015-02-20 02:10:56 +0000

    Shuffle when clicking shuffle

--- app/js/app.js
+++ app/js/app.js
@@ -1,12 +1,5 @@
-import NewwwnessApi from './newwwness-api'
-import formatArticles from './format-articles'
 import Loader from './loader'
 
-Loader.start()
-
-NewwwnessApi.load()
-  .then(formatArticles)
-  .then(Loader.stop())
-  .catch(err => console.log(err))
+Loader.load()
 
 export default {}
--- app/js/loader.js
+++ app/js/loader.js
@@ -1,6 +1,10 @@
+import NewwwnessApi from './newwwness-api'
+import formatArticles from './format-articles'
+
 class Loader {
   constructor() {
     this.el = document.getElementById('loader')
+    this.el.addEventListener('click', this.load.bind(this))
   }
 
   start() {
@@ -10,6 +14,17 @@ class Loader {
   stop() {
     this.el.classList.remove('is-loading');
   }
+
+  load() {
+    this.el.classList.add('is-loading')
+
+    NewwwnessApi.load()
+      .then(formatArticles)
+      .then(() => this.el.classList.remove('is-loading'))
+      .catch(err => console.log(err))
+  }
 }
 
 export default new Loader
+
+
