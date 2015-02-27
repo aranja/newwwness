d84843bdf2a2d373dd8c936d46afa60e44bc0dbc
David Blurton <david.blurton@gmail.com>
2015-02-27 17:11:44 +0000

    Enable CORS on the server

--- api/server.go
+++ api/server.go
@@ -2,11 +2,13 @@ package newwwness
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
 	"net/http"
 )
 
 func init() {
-	http.Handle("/api/", CreateHandler())
+	c := cors.New(cors.Options{})
+	http.Handle("/api/", c.Handler(CreateHandler()))
 }
 
 func CreateHandler() http.Handler {
@@ -16,8 +18,8 @@ func CreateHandler() http.Handler {
 	api.HandleFunc("/random", RandomArticles)
 	api.HandleFunc("/new", NewArticles)
 
-	api.HandleFunc("/articles", makeHandler(getAllArticles)).Methods("GET")
-	api.HandleFunc("/articles/{id}", makeHandlerWithParameter(getArticle)).Methods("GET")
+	//api.HandleFunc("/articles", makeHandler(getAllArticles)).Methods("GET")
+	//api.HandleFunc("/articles/{id}", makeHandlerWithParameter(getArticle)).Methods("GET")
 	//api.HandleFunc("/articles", makeHandler(createArticle)).Methods("POST")
 	//api.HandleFunc("/articles/{id}", makeHandler(updateArticle)).Methods("PUT")
 	//api.HandleFunc("/articles/{id}", makeHandlerWithParameter(deleteArticle).Methods("DELETE"))
