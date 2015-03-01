eca90fa0ea52b5776ed8da8e62730b1849c81891
David Blurton <david.blurton@gmail.com>
2015-03-01 15:41:50 +0000

    Formatting

--- app/public/background.js
+++ app/public/background.js
@@ -1,21 +1,21 @@
 chrome.webRequest.onBeforeSendHeaders.addListener(function(details) {
-    var requestHeaders = details.requestHeaders;
-    for (var i=0; i<requestHeaders.length; ++i) {
-        if (requestHeaders[i].name.toLowerCase() === 'referer') {
-            // The request was certainly not initiated by a Chrome extension...
-            return;
-        }
+  var requestHeaders = details.requestHeaders;
+  for (var i = 0; i < requestHeaders.length; ++i) {
+    if (requestHeaders[i].name.toLowerCase() === 'referer') {
+      // The request was certainly not initiated by a Chrome extension...
+      return;
     }
-    // Set Referer
-    requestHeaders.push({
-        name: 'referer',
-        // Host must match the domain in your Typekit kit settings
-        value: 'http://newwwness.co/'
-    });
-    return {
-        requestHeaders: requestHeaders
-    };
+  }
+  // Set Referer
+  requestHeaders.push({
+    name: 'referer',
+    // Host must match the domain in your Typekit kit settings
+    value: 'http://newwwness.co/'
+  });
+  return {
+    requestHeaders: requestHeaders
+  };
 }, {
-    urls: ['*://use.typekit.net/*'],
-    types: ['stylesheet']
-}, ['requestHeaders','blocking']);
+  urls: ['*://use.typekit.net/*'],
+  types: ['stylesheet']
+}, ['requestHeaders', 'blocking']);
