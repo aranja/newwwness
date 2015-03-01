6bb4a07176e9f82f9d9f70156dcef738d985d584
David Blurton <david.blurton@gmail.com>
2015-03-01 15:40:16 +0000

    Formatting

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,9 @@
 import Loader from './loader'
 import DomReady from './dom-ready'
 
-try{Typekit.load();}catch(e){}
+try {
+  Typekit.load()
+} catch () {}
 
 DomReady(() => {
   let loader = new Loader()
