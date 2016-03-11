7c94f009ad8e1cc8af946d15b6d617a98a8f293d
Andy Clabaugh <andy@upperquad.com>
2016-03-11 14:58:58 -0800

    Pull back to top functionality into separate js file

---
+++ app/js/back-to-top.js
@@ -0,0 +1,32 @@
+
+class BackToTop {
+  constructor() {
+    this.backToTop = document.getElementById('backToTop')
+    this.backToTop.addEventListener('click', this.topHandler.bind(this))
+  }
+
+  topHandler() {
+    this.scrollToTop(500)
+  }
+
+  scrollToTop(scrollDuration) {
+    const   scrollHeight = window.scrollY,
+            scrollStep = Math.PI / ( scrollDuration / 15 ),
+            cosParameter = scrollHeight / 2;
+    var     scrollCount = 0,
+            scrollMargin;
+    requestAnimationFrame(step);
+    function step () {
+      setTimeout(function() {
+        if ( window.scrollY > 10 ) {
+          requestAnimationFrame(step);
+          scrollCount = scrollCount + 1;
+          scrollMargin = cosParameter - cosParameter * Math.cos( scrollCount * scrollStep );
+          window.scrollTo( 0, ( scrollHeight - scrollMargin ) );
+        }
+      }, 15 );
+    }
+  }
+}
+
+export default BackToTop
