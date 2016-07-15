2d9fb62230650cfb2511e753a9731b061c3bb50c
Andy Clabaugh <andy@upperquad.com>
2016-07-14 18:08:16 -0700

    Remove marketing stuff

--- marketing/gulp/tasks/default.js
+++
@@ -1,6 +0,0 @@
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-
-gulp.task('default', function(cb) {
-  runSequence(['sass', 'scripts'], 'copy', 'browser-sync', 'watch', cb);
-});
